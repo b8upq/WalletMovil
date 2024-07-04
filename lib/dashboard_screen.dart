783ff32c7232f3cce92fb390fe8e3b1eb70d38f4
uzielR <uziiel2a2@gmail.com>
2024-07-03 18:25:27 -0600

    se agrego la navbar a la principal
    agregue la navbar en main

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -1,17 +1,57 @@
 import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Inicio'),
+    Text('Esta es la vista de perfil.'),
+    Text('Carrito de compras'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Mi Perfil'),
+        title: const Text('App con Barra de Navegación'),
       ),
-      body: const Center(
-        child: Text('Esta es la vista de perfil.'),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Carrito',
+          )
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color.fromARGB(255, 255, 162, 0),
+        unselectedItemColor: Colors.black,
+        onTap: _onItemTapped,
       ),
     );
   }
 }
+
